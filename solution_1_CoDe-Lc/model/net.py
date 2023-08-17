d27e557f9eff3f09d17f66e4d54e0112d580c63c
Diverting Pan <39986072+divertingPan@users.noreply.github.com>
2023-08-17 17:12:25 +0800

    Add files via upload

---
+++ solution_1_CoDe-Lc/model/net.py
@@ -0,0 +1,193 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+from model.minifasnet import MiniFASNetV2SE
+
+
+class ResNet_x128(nn.Module):
+    def __init__(self):
+        super(ResNet_x128, self).__init__()
+        self.resnet_model = models.alexnet(num_classes=128)
+        self.score_layer = nn.Linear(128, 1)
+
+    def forward(self, x):
+        features = self.resnet_model(x)
+        features = torch.sigmoid(features)
+        score = self.score_layer(features)
+        if self.training:
+            # features = torch.sigmoid(features)
+            return features, score
+        else:
+            return score
+
+
+class EnsembleNet(nn.Module):
+    def __init__(self):
+        super(EnsembleNet, self).__init__()
+        # self.vgg_model = models.vgg11_bn(num_classes=128)
+        self.vgg_model = models.alexnet(num_classes=128)
+        self.alexnet_model = models.alexnet(num_classes=128)
+        self.alex_score_layer = nn.Linear(128, 1)
+        self.vgg_score_layer = nn.Linear(128, 1)
+        self.output_layer = nn.Linear(2, 1)
+        self.relu = nn.ReLU()
+        self.tanh = nn.Tanh()
+
+    def forward(self, x):
+        vgg_features = self.vgg_model(x)
+        alexnet_features = self.alexnet_model(x)
+        vgg_features = self.tanh(vgg_features)
+        alexnet_features = self.tanh(alexnet_features)
+        vgg_score = self.vgg_score_layer(self.relu(vgg_features))
+        alex_score = self.alex_score_layer(self.relu(alexnet_features))
+        combined_score = torch.cat((vgg_score, alex_score), dim=1)
+        score = self.output_layer(combined_score)
+        # score = torch.sigmoid(score)
+        if self.training:
+            # vgg_features = torch.tanh(vgg_features)
+            # alexnet_features = torch.tanh(alexnet_features)
+            return vgg_features, alexnet_features, vgg_score, alex_score, score
+        else:
+            return score
+
+
+class EnsembleNet_c4(nn.Module):
+    def __init__(self):
+        super(EnsembleNet_c4, self).__init__()
+        self.vgg_model = models.alexnet(num_classes=128)
+        self.alexnet_model = models.alexnet(num_classes=128)
+        self.alex_score_layer = nn.Linear(128, 1)
+        self.vgg_score_layer = nn.Linear(128, 1)
+        self.output_layer = nn.Linear(2, 1)
+
+    def forward(self, x):
+        vgg_features = self.vgg_model(x)
+        alexnet_features = self.alexnet_model(x)
+        vgg_features = torch.sigmoid(vgg_features)
+        alexnet_features = torch.sigmoid(alexnet_features)
+        vgg_score = self.vgg_score_layer(vgg_features)
+        alex_score = self.alex_score_layer(alexnet_features)
+        combined_score = torch.cat((vgg_score, alex_score), dim=1)
+        score = self.output_layer(combined_score)
+        if self.training:
+            return vgg_features, alexnet_features, vgg_score, alex_score, score
+        else:
+            return score
+
+
+class PixelNet(nn.Module):
+    def __init__(self, num_classes: int = 1, dropout: float = 0.5) -> None:
+        super().__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(256, 1, kernel_size=1),
+        )
+        # self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=dropout),
+            # nn.Linear(27 * 27, 4096),
+            # nn.ReLU(inplace=True),
+            # nn.Dropout(p=dropout),
+            # nn.Linear(4096, 4096),
+            # nn.ReLU(inplace=True),
+            nn.Linear(26 * 26, num_classes),
+        )
+
+    def forward(self, img):
+        map = self.features(img)
+        map = torch.sigmoid(map)
+        # x = self.avgpool(map)
+        x = torch.flatten(map, 1)
+        x = self.classifier(x)
+        x = torch.sigmoid(x)
+        return map, x
+
+
+class FTGenerator(nn.Module):
+    def __init__(self, in_channels=48, out_channels=1):
+        super(FTGenerator, self).__init__()
+
+        self.ft = nn.Sequential(
+            nn.Conv2d(in_channels, 128, kernel_size=(3, 3), padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(128, 64, kernel_size=(3, 3), padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(64, out_channels, kernel_size=(3, 3), padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.ft(x)
+
+
+class MultiFTNet(nn.Module):
+    def __init__(self, img_channel=3, num_classes=1, embedding_size=128, conv6_kernel=(5, 5)):
+        super(MultiFTNet, self).__init__()
+        self.img_channel = img_channel
+        self.num_classes = num_classes
+        self.model = MiniFASNetV2SE(embedding_size=embedding_size, conv6_kernel=conv6_kernel,
+                                    num_classes=num_classes, img_channel=img_channel, drop_p=0.8)
+        self.FTGenerator = FTGenerator(in_channels=128)
+        self._initialize_weights()
+        # self.vgg_model = models.vgg11(num_classes=1, dropout=0.9)
+        # self.alexnet_model = models.alexnet(num_classes=1, dropout=0.9)
+        # self.drop = torch.nn.Dropout(p=0.2)
+        # self.output_layer = nn.Linear(3, 1)
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, (nn.BatchNorm2d, nn.BatchNorm1d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, std=0.001)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        # alex_o = self.alexnet_model(x)
+        # vgg_o = self.vgg_model(x)
+        x = self.model.conv1(x)
+        x = self.model.conv2_dw(x)
+        x = self.model.conv_23(x)
+        x = self.model.conv_3(x)
+        x = self.model.conv_34(x)
+        x = self.model.conv_4(x)
+        x1 = self.model.conv_45(x)
+        x1 = self.model.conv_5(x1)
+        x1 = self.model.conv_6_sep(x1)
+        x1 = self.model.conv_6_dw(x1)
+        x1 = self.model.conv_6_flatten(x1)
+        x1 = self.model.linear(x1)
+        x1 = self.model.bn(x1)
+        x2 = self.model.drop(x1)
+        cls = self.model.prob(x2)
+        # cls = self.drop(torch.cat((alex_o, vgg_o, fas_o), dim=1))
+        # cls = self.output_layer(cls)
+
+        if self.training:
+            f_m = self.FTGenerator(x)
+            return cls, x1, f_m
+        else:
+            return cls
---
+++ solution_1_CoDe-Lc/test.py
@@ -0,0 +1,123 @@
+import numpy as np
+import os
+import argparse
+import random
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import torch
+from torch.utils.data import DataLoader
+import torch.backends.cudnn as cudnn
+from torchvision import models
+from torch.utils.tensorboard import SummaryWriter
+
+from dataset import TestDataset
+from utils import performances_cross_db
+from model.net import EnsembleNet
+
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # if you are using multi-GPU.
+    np.random.seed(seed)  # Numpy module.
+    random.seed(seed)  # Python random module.
+
+    # torch.use_deterministic_algorithms(True, warn_only=True)
+    torch.backends.cudnn.enabled = True
+    torch.backends.cudnn.benchmark = True
+    os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
+    os.environ['PYTHONHASHSEED'] = str(seed)
+
+
+def test_on_dataset(model, epoch, test_loader, test_label, writer, device, cen_criterion, mse_loss):
+    model.eval()
+    raw_test_scores, gt_labels = [], []
+    mean_loss_bce, mean_loss_mse = [], []
+    # mean_loss_bce_map, mean_loss_mse_map = [], []
+    with torch.no_grad():
+        for i, data in enumerate(test_loader):
+            img, label = data["images"].to(device), data["labels"].to(device)
+
+            # mag, phase = torch.chunk(fft, 2, dim=-3)
+            raw_scores = model(img)
+
+            mean_loss_bce.append(cen_criterion(raw_scores.squeeze(), label).item())
+            mean_loss_mse.append(mse_loss(raw_scores.squeeze(), label).item())
+            # mean_loss_bce_map.append(cen_criterion(map_output.squeeze(), map_label).item())
+            # mean_loss_mse_map.append(mse_loss(map_output.squeeze(), map_label).item())
+
+            # mean_map_scores = torch.mean(map_output, dim=[1, 2, 3]).cpu().data.numpy()
+            raw_scores = raw_scores.cpu().data.numpy()
+            raw_test_scores.extend(raw_scores)
+            gt_labels.extend(label.cpu().data.numpy())
+
+        performances_cross_db(raw_test_scores, gt_labels, writer=writer, epoch=epoch, test_label=test_label)
+
+        # writer.add_image(f'test_{test_label}/images', make_grid(img), epoch)
+        # writer.add_image(f'test_{test_label}/magnitude', make_grid(mag), epoch)
+        # writer.add_image(f'test_{test_label}/phase', make_grid(phase), epoch)
+
+        writer.add_scalar(f'test_{test_label}/bce_loss', np.mean(mean_loss_bce), epoch)
+        writer.add_scalar(f'test_{test_label}/mse_loss', np.mean(mean_loss_mse), epoch)
+        # writer.add_scalar(f'test_{test_label}/map_bce_loss', np.mean(mean_loss_bce_map), epoch)
+        # writer.add_scalar(f'test_{test_label}/mse_map', np.mean(mean_loss_mse_map), epoch)
+
+        # Add the raw_test_scores to TensorBoard
+        fig, ax = plt.subplots()
+        ax.plot(raw_test_scores)
+        ax.set_xlabel('item')
+        ax.set_ylabel('score')
+        writer.add_figure(f'test_{test_label}/scores', fig, epoch)
+
+        return gt_labels, raw_test_scores
+
+
+if __name__ == '__main__':
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # device = 'cpu'
+
+    torch.cuda.empty_cache()
+    set_seed(seed=777)
+
+    parser = argparse.ArgumentParser(description='SynPAD Training with MixStyle')
+    parser.add_argument("--prefix", default='casia-cefa', type=str, help="testing dataset")
+    parser.add_argument("--weight_path", default='checkpoints/ckpt.pth', type=str, help="model weights")
+    parser.add_argument("--csv_test", default='dataset/CASIA-CEFA/test.csv', type=str, help="csv contains training data")
+    parser.add_argument("--input_shape", default=(224, 224), type=tuple, help="Neural Network input shape")
+    parser.add_argument("--batch_size", default=128, type=int, help="train batch size")
+
+    args = parser.parse_args()
+
+    if not os.path.isdir('logs'):
+        os.makedirs('logs')
+
+    writer = SummaryWriter(log_dir='logs/test_{}'.format(args.prefix))
+
+    test_dataset = TestDataset(csv_file=args.csv_test, input_shape=args.input_shape)
+    test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=0, pin_memory=True)
+
+    model = EnsembleNet()
+    # model = torch.nn.DataParallel(model)
+    model = model.to(device)
+    model.load_state_dict(torch.load(args.weight_path))
+
+    # cen_criterion = torch.nn.BCELoss().to(device)
+    cen_criterion = torch.nn.BCEWithLogitsLoss().to(device)
+    mse_loss = torch.nn.MSELoss().to(device)
+
+    model.eval()
+    true_label, prediction_score = test_on_dataset(model, 0, test_loader, args.prefix,
+                                                   writer, device, cen_criterion, mse_loss)
+
+    # image_path, true_label, prediction_score, prediction_label
+    dataframe = pd.read_csv(args.csv_test)
+    image_path = dataframe.iloc[:, 0].values.tolist()
+    # true_label = gt_labels
+    # prediction_score = raw_test_scores
+    prediction_label = [1 if p >= 0 else 0 for p in prediction_score]
+
+    total_df = pd.DataFrame({'image_path': image_path, 'true_label': true_label,
+                             'prediction_score': prediction_score,
+                             'prediction_label': prediction_label})
+    total_df.to_csv('prediction.csv', index=False)
